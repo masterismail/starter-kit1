1908b1c2ec03a5dcdcdd5a397218cc273de0a7c3
Mohammed Ismail <mohammedismail523261@gmail.com>
2024-01-29 20:53:55 +0530

    updated footer

--- packages/blog-starter-kit/themes/magazine/components/publication-footer.tsx
+++ packages/blog-starter-kit/themes/magazine/components/publication-footer.tsx
@@ -1,86 +1,152 @@
-// MobX Stuff
-import Image from 'next/legacy/image';
-
-import { HashnodeLogoIconV2 } from './icons/svgs';
-import { resizeImage } from '../utils/image';
 import Link from 'next/link';
+import { Container } from './container';
+import { useAppContext } from './contexts/appContext';
+import { SocialLinks } from './social-links';
+import PublicationSocialLinks from './publication-social-links';
 
-// type PublicationFooterProps = Pick<Publication, 'title' | 'postsCount' | 'imprint' | 'isTeam'> &
-//   Pick<Publication['preferences'], 'disableFooterBranding' | 'logo' | 'darkMode'> & {
-//     authorName: string;
-//   }; // TODO: types need to be fixed
-
-function PublicationFooter(props: any) {
-  const { isTeam, authorName, title, imprint, disableFooterBranding, logo } = props;
-
-  return (
-    <footer className="blog-footer-area -mt-px border-t bg-slate-100 px-5 py-10 text-center text-slate-800 dark:border-slate-800 dark:bg-black dark:text-slate-500 md:px-10 md:py-12 lg:py-20">
-      {imprint && (
-        <section className="blog-impressum mx-auto mb-10 rounded-lg border bg-white px-4 py-6 text-left dark:border-slate-800 dark:bg-transparent lg:w-3/4 xl:w-2/3">
-          <p className="mb-4 text-center text-sm font-bold uppercase tracking-wider text-slate-600 dark:text-slate-400">
-            Impressum
-          </p>
-          {/* eslint-disable-next-line react/self-closing-comp */}
-          <div
-            className="prose mx-auto w-full dark:prose-dark"
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{ __html: `${imprint}` }}
-          ></div>
-        </section>
-      )}
-      <div className="blog-footer-credits flex flex-col items-center justify-center">
-        <div className="mb-12 flex flex-col flex-wrap items-center">
-          <p className="mb-2 text-slate-600 dark:text-slate-300">
-            &copy;{new Date().getFullYear()} {title || `${authorName}'s Blog`}
-          </p>
-          <div className="flex flex-row items-center text-slate-600 dark:text-slate-300">
-            <a href="https://hashnode.com/privacy?source=blog-footer" className="mx-2 underline">
-              Privacy policy
-            </a>
-            <span className="font-extrabold text-black opacity-20 dark:text-white">&middot;</span>
-            <a className="mx-2 underline" href="https://hashnode.com/terms?source=blog-footer">
-              Terms
-            </a>
-          </div>
-        </div>
-        {disableFooterBranding ? (
-          <>
-            {logo && (
-              <div className="flex flex-col items-center">
-                <Link href="/" className="relative block h-10 w-40">
-                  <Image
-                    layout="fill"
-                    alt={title || `${authorName}'s ${isTeam ? 'team' : ''} blog`}
-                    src={resizeImage(logo, { w: 1000, h: 250, c: 'thumb' })}
-                  />
-                </Link>
-              </div>
-            )}
-          </>
-        ) : (
-          <div className="flex flex-col items-center">
-            <Link
-              aria-label="Publish with Hashnode"
-              className="mb-4 flex flex-row items-center rounded-lg border border-slate-300 bg-white p-3 font-heading font-bold tracking-wide text-slate-600 transition-colors duration-75 hover:border-slate-400 hover:text-slate-900 dark:border-slate-800 dark:bg-black dark:text-slate-300 dark:hover:border-slate-500 dark:hover:text-white"
-              href="https://hashnode.com/onboard?unlock-blog=true&source=blog-footer"
-            >
-              <span className="mr-2 block text-blue-600">
-                <HashnodeLogoIconV2 className="h-6 w-6 fill-current" />
-              </span>
-              <span>Publish with Hashnode</span>
-            </Link>
-            <p className="text-sm text-slate-600 dark:text-slate-300">
-              Powered by{' '}
-              <a aria-label="Hashnode" href="https://hashnode.com?source=blog-footer" className="underline">
-                Hashnode
-              </a>{' '}
-              - Home for tech writers and readers
-            </p>
-          </div>
-        )}
-      </div>
-    </footer>
-  );
-}
+export const PublicationFooter = () => {
+	const { publication } = useAppContext();
+	const PUBLICATION_LOGO = publication.preferences.logo;
+	return (
+		<footer className="border-t py-20 dark:border-neutral-800 ">
+			<Container className="px-5">
+				{PUBLICATION_LOGO ? (
+					<div className="mb-20 flex w-full flex-row justify-center">
+						<Link
+							href={'/'}
+							aria-label={`${publication.title} home page`}
+							className="flex flex-row items-center gap-5"
+						>
+							<img className="block w-40" src={PUBLICATION_LOGO} alt={publication.title} />
+						</Link>
+					</div>
+				) : (
+					<p className="mb-20 text-center text-xl font-semibold text-slate-900 dark:text-slate-50 md:text-4xl">
+						{publication.title}
+					</p>
+				)}
+				<div className="grid w-full grid-cols-3 gap-5 md:grid-cols-6 lg:grid-cols-5">
+					<div className="col-span-1 grid grid-cols-4 gap-5 md:col-span-4 lg:col-span-3">
+						<div className="col-span-full md:col-span-2 lg:col-span-1">
+							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">
+								Stay in touch
+							</p>
+							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
+								<li>
+									<a href="#" className="hover:underline">
+										Contact us
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Book a demo
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Newsletter
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Slack
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div className="col-span-full md:col-span-2 lg:col-span-1">
+							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">Resources</p>
+							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
+								<li>
+									<a href="#" className="hover:underline">
+										Community
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Use Cases
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Source Code
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Blog
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div className="col-span-full md:col-span-2 lg:col-span-1">
+							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">Product</p>
+							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
+								<li>
+									<a href="#" className="hover:underline">
+										Pricing
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Documentation
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Integrations
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Support
+									</a>
+								</li>
+							</ul>
+						</div>
+						<div className="col-span-1">
+							<p className="mb-2 font-semibold text-slate-600 dark:text-neutral-200">Other links</p>
+							<ul className="flex flex-col gap-1 text-slate-700 dark:text-neutral-300">
+								<li>
+									<a href="#" className="hover:underline">
+										Events
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Careers
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Newsroom
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										About us
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="col-span-2 flex flex-col items-end gap-5 text-right text-slate-600 dark:text-neutral-300 md:text-left">
+						<SocialLinks />
+						<p>&copy; 2023 Company Inc.</p>
+						<p>
+							<a href="#" className="hover:underline">
+								Privacy Policy
+							</a>{' '}
+							·{' '}
+							<a href="#" className="hover:underline">
+								Terms
+							</a>
+						</p>
+					</div>
+				</div>
+			</Container>
+		</footer>
+	);
+};
 
 export default PublicationFooter;
